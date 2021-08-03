e4f5ed921c64e85b8b16f10780f4ef4b75a77392
castrokevyn <kevyn.castro@zipeli.com>
2021-08-03 14:49:43 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
